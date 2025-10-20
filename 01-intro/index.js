c547aa85e9811380107fc584de13e0563b9d0f3a
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-20 14:43:25 -0300

    intro

---
+++ 01-intro/index.js
@@ -0,0 +1,7 @@
+// var firstName = "Arthur";
+// var anothername = 1;
+// var x = true;
+// function greeting(name) {
+//     console.log("olá, " + name);
+// }
+// greeting(firstName);
---
+++ 01-intro/index.ts
@@ -0,0 +1,9 @@
+const firstName = "Arthur"
+const anothername = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("olá" +name)
+}
+
+greeting(firstName)
