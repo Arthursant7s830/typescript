2f594fc2f00338fad23abd0fe564fc9fa8311277
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-20 18:49:49 -0300

    String

---
+++ 02-fundamentos/02-string/index.js
@@ -0,0 +1,7 @@
+var firstName = "Arthur";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Lima";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
---
+++ 02-fundamentos/02-string/index.ts
@@ -0,0 +1,10 @@
+const firstName: string = "Arthur"
+console.log(firstName.toUpperCase())
+
+let fullName: string 
+const lastName: string = "Lima"
+
+fullName = firstName + " " + lastName
+console.log(fullName)
+
+console.log(typeof fullName)
