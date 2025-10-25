274a69dce1f9b0ded785df836db676e1b9e7011f
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-25 11:35:13 -0300

    arrays 01

---
+++ 03-avancando-em-tipos/01-arrays/index.js
@@ -0,0 +1,3 @@
+var numbers = [1, 2, 3, 4];
+numbers.push(5, 9);
+console.log(numbers[5]);
---
+++ 03-avancando-em-tipos/01-arrays/index.ts
@@ -0,0 +1,6 @@
+let numbers: number[] = [1, 2, 3, 4]
+numbers.push(5, 9)
+
+console.log(numbers[5])
+
+const nomes: string[] = ["Arthur"]
