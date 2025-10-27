efc2fe227c66b7379b634053e6c2471726fdfb4b
Arthursant7s830 <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-27 15:47:24 -0300

    tipos de objetos

---
+++ 03-avancando-em-tipos/05-tipos-objetos/index.js
@@ -0,0 +1,6 @@
+function passCordinates(coord) {
+    console.log("X coordinates: " + coord.x);
+    console.log("y coordinates: " + coord.y);
+}
+var objCoord = { x: 329, y: 84.2 };
+passCordinates(objCoord);
---
+++ 03-avancando-em-tipos/05-tipos-objetos/index.ts
@@ -0,0 +1,8 @@
+function passCordinates(coord: {x: number, y: number}){ 
+console.log("X coordinates: " + coord.x)
+console.log("y coordinates: " + coord.y)
+}  
+
+const objCoord = {x: 329, y: 84.2}
+
+passCordinates(objCoord)
