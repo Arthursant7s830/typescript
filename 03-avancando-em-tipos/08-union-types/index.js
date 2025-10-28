b073f0f0c70661bc5617b6474ae1d45fe84f0b51
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-28 12:41:55 -0300

    union types

---
+++ 03-avancando-em-tipos/08-union-types/index.js
@@ -0,0 +1,5 @@
+function showBalance(balance) {
+    console.log("O saldo da conta \u00E9 de R$".concat(balance));
+}
+showBalance(100);
+showBalance("500");
---
+++ 03-avancando-em-tipos/08-union-types/index.ts
@@ -0,0 +1,6 @@
+function showBalance(balance: string | number) {
+    console.log(`O saldo da conta é de R$${balance}`)
+}
+
+showBalance(100)
+showBalance("500")
