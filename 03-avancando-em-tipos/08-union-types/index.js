6bc433d607b73ea77ae89e9d103f58177b316e40
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-28 13:03:53 -0300

    avançando em union types

--- 03-avancando-em-tipos/08-union-types/index.js
+++ 03-avancando-em-tipos/08-union-types/index.js
@@ -4,3 +4,12 @@ function showBalance(balance) {
 showBalance(100);
 showBalance("500");
 var arr2 = [1, "teste"];
+// AVANÇANDO EM UNION TYPES
+function showUserRole(role) {
+    if (typeof role === "boolean") {
+        return "Usuário não aprovado!";
+    }
+    return "A fun\u00E7\u00E3o do usu\u00E1rio \u00E9: ".concat(role);
+}
+console.log(showUserRole(true));
+console.log(showUserRole("Admin"));
--- 03-avancando-em-tipos/08-union-types/index.ts
+++ 03-avancando-em-tipos/08-union-types/index.ts
@@ -7,3 +7,16 @@ showBalance("500")
 
 
 const arr2: Array<number | string | boolean> = [1, "teste"]
+
+// AVANÇANDO EM UNION TYPES
+
+function showUserRole(role: boolean | String) {
+    if(typeof role === "boolean") {
+        return "Usuário não aprovado!"
+    }
+
+    return `A função do usuário é: ${role}` 
+}
+
+console.log(showUserRole(true))
+console.log(showUserRole("Admin"))
