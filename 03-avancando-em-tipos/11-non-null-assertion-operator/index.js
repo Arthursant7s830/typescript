c1137f263b59a385e2237fc49dd72d153344c0bf
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-30 18:29:39 -0300

    symbol

--- 03-avancando-em-tipos/11-non-null-assertion-operator/index.js
+++ 03-avancando-em-tipos/11-non-null-assertion-operator/index.js
@@ -3,3 +3,9 @@ console.log(p.innerText);
 // BIGINT
 var num = 1000n;
 console.log(typeof num);
+console.log(num);
+// SYMBOL 
+var symbolA = Symbol("a");
+var symbolB = Symbol("b");
+console.log(symbolA == symbolB);
+console.log(symbolA === symbolB);
--- 03-avancando-em-tipos/11-non-null-assertion-operator/index.ts
+++ 03-avancando-em-tipos/11-non-null-assertion-operator/index.ts
@@ -9,3 +9,10 @@ const num: bigint = 1000n
 console.log(typeof num)
 console.log(num)
  
+// SYMBOL 
+
+let symbolA: symbol = Symbol("a")
+let symbolB = Symbol("b")
+
+console.log(symbolA == symbolB)
+console.log(symbolA === symbolB)
