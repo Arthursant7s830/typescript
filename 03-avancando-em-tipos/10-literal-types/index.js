fd8f30d9c4ba0230fd01bc2b35ca73d5307ff5f7
Arthur Santos Lima <135383243+Arthursant7s830@users.noreply.github.com>
2025-10-30 14:19:11 -0300

    literal types

---
+++ 03-avancando-em-tipos/10-literal-types/index.js
@@ -0,0 +1,6 @@
+var test;
+test = "testando";
+function showDirection(direction) {
+    console.log("A dire\u00E7\u00E3o \u00E9 ".concat(direction));
+}
+showDirection("center");
---
+++ 03-avancando-em-tipos/10-literal-types/index.ts
@@ -0,0 +1,9 @@
+let test: "testando"
+
+test = "testando"
+
+function showDirection (direction: "left" | "right" | "center") {
+    console.log(`A direção é ${direction}`)
+}
+
+showDirection("center")
